71be47d12d76d0f0b1145e6ec0ee48bf8d679c36
amunium <age7@hotmail.no>
2021-11-15 15:20:42 +0100

    start ignore database.db

--- ban.sh
+++ ban.sh
@@ -1,7 +1,11 @@
 # Denne kalles med en ipadresse som skal bli bannet som argument (sjekk "calling bash file with argument") og lagrer dette med timestamp i database.db
 
+# Henter argumentet den henter fra
 ipAdress=$1
 
+echo $ipAdress
 
 
-cat $ipAdress >> database.db
+
+
+echo $ipAdress >> database.db
