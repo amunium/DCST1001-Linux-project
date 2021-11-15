558fce49677d16da80ad8593a8482aa8a06d83d9
Amunium <age7@hotmail.no>
2021-11-15 14:47:52 +0100

    add gitignore

--- ban.sh
+++ ban.sh
@@ -1,1 +1,7 @@
 # Denne kalles med en ipadresse som skal bli bannet som argument (sjekk "calling bash file with argument") og lagrer dette med timestamp i database.db
+
+ipAdress=$1
+
+
+
+cat $ipAdress >> database.db
