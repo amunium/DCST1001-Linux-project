5198b45bb7cfa532cb5f33946ef75b4f607ef46e
amunium <age7@hotmail.no>
2021-11-15 14:19:50 +0100

    add pseudo code

--- miniban.sh
+++ miniban.sh
@@ -1,1 +1,14 @@
 # Denne skal lytte etter ssh tilkoblinger og kalle "ban.sh" når den oppdager 3 fails
+
+trap "./unban" EXIT
+
+while(true)
+    journalctl -u ssh
+
+    tempfile = mkdir $tempfile
+
+    trap "./ban ipadresse" EXIT
+
+
+
+trap "" EXIT
