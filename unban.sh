7e4de2d9e3ba99a5e3d04183d70041578a995355
Amunium <age7@hotmail.no>
2021-11-17 14:30:41 +0100

    add pseudocode unban.sh

--- unban.sh
+++ unban.sh
@@ -4,3 +4,28 @@
 # Muligens jeg hjelper deg litt:
 # Dette kan være kommandoen for for unban hvor "$ipAdress" er en variabel som skal inneholde ipen du hentet fra database.db som skal unbannes
 # iptables -D INPUT -s $ipAdress -j DROP # Tror det er et problem med den, men skal ikke være for vanskelig å finne ut av det.
+
+while true
+do 
+    # måles i sekunder
+    sleep 1
+    # check if any ip is 10 min old (600 sec)
+    echo "Checking if any banned IP address is too old"
+    
+    time = $(date +%s)
+
+    # grep for å finne tid spesifikt: grep [0-9]{4,} <her setter du inn det du skal sjekke>
+
+    # TODO: for loop
+    for each line in miniban.db 
+        ipTime = #TODO
+        oldTime = $((time - 600))
+
+
+        if [$newTime -le $oldTime ]
+        then
+            echo "Removed ban on: "$ipAdress
+            iptables -D INPUT -s $ipAdress -j DROP
+            # TODO remove from database
+        fi
+done
